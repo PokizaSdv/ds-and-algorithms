9108f97d10ea489ad01362e0684a816abcc2334c
PokizaSdv <pokizasdv@gmail.com>
2024-02-18 08:24:48 -0500

    Get sum of even idx number and all numbers in the array in 2 ways

--- chapter-5/index.js
+++ chapter-5/index.js
@@ -64,3 +64,37 @@ function doubleThenSum2(array) {
     return array.map(num => num * 2).reduce((acc, curr) => acc + curr, 0)
 }
 console.log(doubleThenSum2([2,4,6]))
+
+//function which accepts an array of strings and prints each string in multiple cases
+function multipleCases(array) {
+    for(const str of array) {
+        console.log(str.toUpperCase())
+        console.log(str.toLowerCase())
+        console.log(str[0].toUpperCase() + str.slice(1))
+    }
+}
+multipleCases(["apple", "banana", "lemon"])
+
+//function, that returns sum of each number in even index and all numbers in the array
+function sumOfEvenIdxNumAndAll(array) {
+    for(let i = 0; i < array.length; i++) {
+        if(i % 2 === 0) {
+            let sum = array[i];
+            for(let j = 0; j < array.length; j++){
+                sum += array[j]
+            }
+            console.log(`Sum of ${array[i]} and all number in the array: ${sum}`)
+        }
+    }
+}
+sumOfEvenIdxNumAndAll([1,2,3,4,5,6])
+
+function sumOfEvenIdxNumAndAll2(array) {
+    array.forEach((number, index) => {
+        if(index % 2 === 0) {
+            const sum = array.reduce((acc, cur) => acc + cur, number)
+            console.log(`Sum of ${number} and all numbers in the array: ${sum}`)
+        }
+    })
+}
+sumOfEvenIdxNumAndAll2([1,2,3,4,5,6])
